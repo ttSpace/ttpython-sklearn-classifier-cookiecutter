6b9f165502b84e651dcc714699327debd05d673a
ttSpace <82852072+ttSpace@users.noreply.github.com>
2021-11-10 14:15:04 +0800

    Update classifier.py

--- {{cookiecutter.app_name}}/classifier.py
+++ {{cookiecutter.app_name}}/classifier.py
@@ -317,3 +317,4 @@ if __name__ == '__main__':
     print("Oct 13th, Cold is coming.")
     print("Oct 27th, the https://github.com/microsoft/PTVS/issues/6684 was not happen.")
     print("Nov 8th, the Cookiecutter works fine, the python installed from microsoft store.")
+    print("Nov 10th, Dark Before Home.")
