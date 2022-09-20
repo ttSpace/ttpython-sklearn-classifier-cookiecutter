fdecf585612ce0aae9a36e11025d408985ff0f70
ttSpace <82852072+ttSpace@users.noreply.github.com>
2022-09-20 14:01:58 +0800

    Update classifier.py

--- {{cookiecutter.app_name}}/classifier.py
+++ {{cookiecutter.app_name}}/classifier.py
@@ -338,3 +338,4 @@ if __name__ == '__main__':
     print("August 17th, twists and turns")
     print("August 31st, nitty gritty ")
     print("Sept 14th, typhoon Muifa.")
+    now.strftime("%A, %d %B, %Y at %X")
