0942fa12d18b810ca253f572d44381b7f24383e6
ttSpace <82852072+ttSpace@users.noreply.github.com>
2024-11-28 14:17:17 +0800

    Update classifier.py

--- {{cookiecutter.app_name}}/classifier.py
+++ {{cookiecutter.app_name}}/classifier.py
@@ -301,7 +301,5 @@ if __name__ == '__main__':
     print("playwright")
     print("EOD")
     print("Not all it's cracked up to be.")
-    print("EOD")
-    print("Not all it's cracked up to be.")
 
 
